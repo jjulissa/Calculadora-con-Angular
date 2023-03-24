97b34ba3663036c87ec801fdb775b71f9c792abd
jjulissa <julissajoseph9@gmail.com>
2023-03-24 19:12:23 +0100

    Calculo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'contador-angular';
+  title = 'Calculadora con Angular'; 
+
+  numeroUno: number = 0; 
+  numeroDos: number = 0; 
+  resultado: number = 0; 
+
+  suma():void { 
+    this.resultado = this.numeroUno + this.numeroDos; 
+    console.log(this.numeroUno); 
+    console.log(this.numeroDos); 
+    console.log(this.resultado);
+    
+  } 
+
+  resta(): void {  
+    this.resultado = this.numeroUno - this.numeroDos;  
+
+    console.log(this.numeroUno); 
+    console.log(this.numeroDos); 
+    console.log(this.resultado);
+  } 
+
+  multiplicar():void {  
+    this.resultado = this.numeroUno * this.numeroDos;  
+
+    console.log(this.numeroUno); 
+    console.log(this.numeroDos); 
+    console.log(this.resultado);
+  } 
+
+  dividir():void {  
+    this.resultado = this.numeroUno / this.numeroDos;  
+
+    console.log(this.numeroUno); 
+    console.log(this.numeroDos); 
+    console.log(this.resultado);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
+    FormsModule, 
     AppRoutingModule
   ],
   providers: [],
